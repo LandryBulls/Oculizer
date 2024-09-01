900627d3bc3131701d57323c20b77fcbbc73889f
LandryBulls <bulls409@gmail.com>
2024-09-01 18:40:38 -0400

    working

--- run_tester.py
+++ run_tester.py
@@ -1,18 +1,28 @@
+import os
+import json
+import threading
+import queue
+import numpy as np
 import curses
-from oculizer.light import AudioLightController, load_controller, load_json, load_profile
+
+#from control import load_json, load_profile, load_controller, LightController
+from oculizer.light import LightController, load_controller, load_json, load_profile
+from oculizer.audio import AudioListener
 from oculizer.scenes import SceneManager
 
 stdscr = curses.initscr()
 
 def main():
+    audio_listener = AudioListener()
     scene_manager = SceneManager('scenes')
     scene_manager.set_scene('hell')
 
-    audio_light_controller = AudioLightController('testing', scene_manager)
+    light_controller = LightController(audio_listener, 'testing', scene_manager)
 
     scene_commands = {ord(scene_manager.scenes[scene]['key_command']): scene for scene in scene_manager.scenes}
 
-    audio_light_controller.start()
+    audio_listener.start()
+    light_controller.start()
 
     while True:
         stdscr.clear()
@@ -21,13 +31,11 @@ def main():
         for i, scene in enumerate(scene_manager.scenes):
             stdscr.addstr(i+2, 0, f"{scene} | Commands: {scene_manager.scenes[scene]['key_command']}")
         
-        errors = audio_light_controller.get_errors()
+        # Print any errors from the audio listener
+        errors = audio_listener.get_errors()
         if errors:
             for i, error in enumerate(errors):
-                max_error_length = curses.COLS - 1  # Maximum length of error message
-                error_message = f"Error: {error}"
-                truncated_error = error_message[:max_error_length]
-                stdscr.addstr(i+len(scene_manager.scenes)+3, 0, truncated_error)
+                stdscr.addstr(i+len(scene_manager.scenes)+3, 0, f"Error: {error}")
         
         stdscr.refresh()
 
@@ -36,19 +44,21 @@ def main():
             break
         elif key in scene_commands:
             try:
-                audio_light_controller.change_scene(scene_commands[key])
+                light_controller.change_scene(scene_commands[key])
                 stdscr.addstr(len(scene_manager.scenes)+3, 0, f"Changed to scene: {scene_commands[key]}")
             except Exception as e:
                 stdscr.addstr(len(scene_manager.scenes)+2, 0, f"Error changing scene: {str(e)}")
         elif key == ord('r'):
             scene_manager.reload_scenes()
-            audio_light_controller.change_scene(scene_manager.current_scene['name'])
+            light_controller.change_scene(scene_manager.current_scene['name'])  # Reapply current scene
             stdscr.addstr(len(scene_manager.scenes) + 4, 0, "Scenes reloaded")
         
         stdscr.refresh()
 
-    audio_light_controller.stop()
-    audio_light_controller.join()
+    audio_listener.stop()
+    light_controller.stop()
+    audio_listener.join()
+    light_controller.join()
     curses.endwin()
 
 if __name__ == '__main__':
