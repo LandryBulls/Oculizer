a43e4dde001a8ee9470938dff7ad21f35ab5551f
LandryBulls <bulls409@gmail.com>
2024-09-02 17:51:03 -0400

    working

--- oculizer/__init__.py
+++ oculizer/__init__.py
@@ -1,5 +1,5 @@
 from oculizer.audio import AudioListener
-from oculizer.light import LightController, Oculizer
+from oculizer.light import Oculizer
 from oculizer.light import mapping
 from oculizer.scenes import SceneManager
 from oculizer.config import audio_parameters
