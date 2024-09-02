8b3f492361839e47316b281b65145ad5b71ff494
LandryBulls <bulls409@gmail.com>
2024-09-01 21:22:01 -0400

    add pyqt thingy

--- oculizer/visualizer.py
+++ oculizer/visualizer.py
@@ -9,7 +9,7 @@ class FeatureVisualizer(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Audio Feature Visualizer")
-        self.setGeometry(100, 100, 800, 600)
+        self.setGeometry(100, 100, 1200, 800)  # Increased size for better visibility
 
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
@@ -23,8 +23,24 @@ class FeatureVisualizer(QMainWindow):
             'onset_strength': pg.PlotWidget(title="Onset Strength")
         }
 
-        for plot in self.plots.values():
+        # Set up bar graphs and lock y-axis
+        self.bars = {}
+        for name, plot in self.plots.items():
             layout.addWidget(plot)
+            if name == 'spectral_centroid':
+                self.bars[name] = pg.BarGraphItem(x=[], height=[], width=0.6, brush='b')
+                plot.addItem(self.bars[name])
+                plot.setYRange(0, 4000)  # Lock y-axis from 0 to 4000
+            elif name == 'spectral_contrast':
+                self.bars[name] = pg.BarGraphItem(x=[], height=[], width=0.6, brush='r')
+                plot.addItem(self.bars[name])
+                plot.setYRange(0, 50)
+            elif name == 'mel_spectrogram': 
+                plot.setYRange(0, 3)  # Lock y-axis for mel spectrogram as well
+            elif name == 'onset_strength':
+                self.bars[name] = pg.BarGraphItem(x=[], height=[], width=0.6, brush='g')
+                plot.addItem(self.bars[name])
+                plot.setYRange(0, 1)
 
         # Initialize AudioListener
         self.audio_listener = AudioListener()
@@ -38,17 +54,22 @@ class FeatureVisualizer(QMainWindow):
     def update_plots(self):
         features = self.audio_listener.get_features()
         if features is not None:
+            # Update mel spectrogram (line plot)
             self.plots['mel_spectrogram'].clear()
-            self.plots['mel_spectrogram'].plot(features['mel_spectrogram'].mean(axis=1))
+            mel_data = features['mel_spectrogram'].mean(axis=1)
+            self.plots['mel_spectrogram'].plot(mel_data)
 
-            self.plots['spectral_centroid'].clear()
-            self.plots['spectral_centroid'].plot(features['spectral_centroid'][0])
+            # Update spectral centroid (bar plot)
+            centroid_data = features['spectral_centroid'][0]
+            self.bars['spectral_centroid'].setOpts(x=range(len(centroid_data)), height=centroid_data)
 
-            self.plots['spectral_contrast'].clear()
-            self.plots['spectral_contrast'].plot(features['spectral_contrast'].mean(axis=1))
+            # Update spectral contrast (bar plot)
+            contrast_data = features['spectral_contrast'].mean(axis=1)
+            self.bars['spectral_contrast'].setOpts(x=range(len(contrast_data)), height=contrast_data)
 
-            self.plots['onset_strength'].clear()
-            self.plots['onset_strength'].plot(features['onset_strength'])
+            # Update onset strength (bar plot)
+            onset_data = features['onset_strength']
+            self.bars['onset_strength'].setOpts(x=range(len(onset_data)), height=onset_data)
 
     def closeEvent(self, event):
         self.audio_listener.stop()
