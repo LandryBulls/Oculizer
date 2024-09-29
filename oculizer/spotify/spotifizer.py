05731387b7cff8b715b632c51ea6e81d2c0488e5
LandryBulls <bulls409@gmail.com>
2024-09-29 18:12:47 -0400

    updated spotifizer

--- oculizer/spotify/spotifizer.py
+++ oculizer/spotify/spotifizer.py
@@ -1,177 +1,185 @@
-"""
-Description: Provides Spotifizer class which runs a thread to continuously pull song info from Spotify API.
-Must have a Spotify Premium account, the app on the same device, have set up the app, etc. 
-"""
-
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
+import os
+import json
+import time
 import threading
 import queue
-import time
-import curses
-import os
-
-# get the client id and client secret from the text file
-# get the client id and client secret from the text file
-credspath = os.path.join(os.path.dirname(__file__), '../../spotify_credentials.txt')
-with open(credspath) as f:
-    client_id = f.readline().strip().split(' ')[1]
-    client_secret = f.readline().strip().split(' ')[1]
-    uri = f.readline().split(' ')[1][:-1]
-    username = f.readline().split(' ')[1][:-1]
-    #token = f.readline().split(' ')[1][:-1]
-
-# set the environment variables
-os.environ['SPOTIPY_CLIENT_ID'], os.environ['SPOTIFY_CLIENT_ID'] = client_id, client_id
-os.environ['SPOTIPY_CLIENT_SECRET'], os.environ['SPOTIFY_CLIENT_SECRET'] = client_secret, client_secret
-os.environ['SPOTIPY_REDIRECT_URI'], os.environ['SPOTIFY_REDIRECT_URI'] = uri, uri
-
-auth_manager = SpotifyClientCredentials()
-sp = spotipy.Spotify(auth_manager=auth_manager)
-
-# just add all the scopes
-scopes = ['user-library-read',
-            'user-read-recently-played',
-            'user-top-read',
-            'user-follow-read',
-            'user-read-playback-position',
-            'user-read-playback-state',
-            'user-read-currently-playing',
-            'user-modify-playback-state',
-            'user-read-private',
-            'playlist-read-private',
-            'playlist-read-collaborative',
-            'playlist-modify-public',
-            'playlist-modify-private']
-
-token = SpotifyOAuth(scope=' '.join(scopes)).get_access_token(as_dict=False)
-
-if token:
-    sp = spotipy.Spotify(auth=token)
-    saved_tracks_resp = sp.current_user_saved_tracks(limit=50)
-else:
-    print('Couldn\'t get token for that username')
-
-user = sp.user(username)
-sp.user = user
+from spotipy import Spotify
+from spotipy.oauth2 import SpotifyOAuth
 
 class Spotifizer(threading.Thread):
-    def __init__(self, update_interval=0.1):
+    def __init__(self, client_id, client_secret, redirect_uri, update_interval=0.05, time_offset=1.0):
         super().__init__()
         self.running = threading.Event()
         self.error_queue = queue.Queue()
         self.info_queue = queue.Queue()
-        self.spotify = sp
+        self.auth_manager = self._create_auth_manager(client_id, client_secret, redirect_uri)
+        self.spotify = self._initialize_spotify()
         self.playing = False
-        self.volume = 0
         self.current_track_id = None
         self.current_track_info = None 
         self.artist = None
         self.title = None
         self.update_interval = update_interval
+        self.time_offset = time_offset  # Time offset in seconds
         self.progress = 0
-        self.suggested_scene = 'testing'
+        self.audio_analysis = None
+        self.current_section = None
+        self.last_update_time = 0
+        self.audio_analysis_lock = threading.Lock()
+        self.section_lock = threading.Lock()
+
+    def _create_auth_manager(self, client_id, client_secret, redirect_uri):
+        scopes = ['user-read-playback-state', 'user-modify-playback-state']
+        return SpotifyOAuth(
+            client_id=client_id,
+            client_secret=client_secret,
+            redirect_uri=redirect_uri,
+            scope=' '.join(scopes)
+        )
+
+    def _initialize_spotify(self):
+        token_info = self.auth_manager.get_cached_token()
+        if not token_info or self.auth_manager.is_token_expired(token_info):
+            token_info = self.auth_manager.refresh_access_token(token_info['refresh_token'])
+        return Spotify(auth=token_info['access_token'])
 
     def run(self):
         self.running.set()
-        try:
-            while self.running.is_set():
-                self.update_current_track()
-        except Exception as e:
-            self.error_queue.put(f"Error in Spotifizer: {str(e)}")
+        while self.running.is_set():
+            self._check_token()
+            self.update_current_track()
+            time.sleep(self.update_interval)
+
+    def _check_token(self):
+        if self.auth_manager.is_token_expired(self.auth_manager.get_cached_token()):
+            token_info = self.auth_manager.refresh_access_token(self.auth_manager.get_cached_token()['refresh_token'])
+            self.spotify = Spotify(auth=token_info['access_token'])
 
     def update_current_track(self):
+        current_time = time.time()
+        if current_time - self.last_update_time < self.update_interval:
+            return
+
         current_playback = self.spotify.current_playback()
-        if current_playback is not None:
-            if current_playback['is_playing']:
-                self.playing = True
-                self.current_track_id = current_playback['item']['id']
+        if current_playback is not None and current_playback['is_playing']:
+            self.playing = True
+            new_track_id = current_playback['item']['id']
+            self.progress = current_playback['progress_ms'] + int(self.time_offset * 1000)  # Add offset
+
+            if new_track_id != self.current_track_id:
+                self.current_track_id = new_track_id
                 self.current_track_info = current_playback['item']
-                self.artist = current_playback['item']['artists'][0]['name']
-                self.title = current_playback['item']['name']
-                self.volume = current_playback['device']['volume_percent']
-                self.progress = current_playback['progress_ms'] + 500
+                self.artist = self.current_track_info['artists'][0]['name']
+                self.title = self.current_track_info['name']
+                self.fetch_audio_analysis()
+
+            self.update_current_section()
         else:
             self.playing = False
             self.current_track_id = None
             self.current_track_info = None
+            with self.audio_analysis_lock:
+                self.audio_analysis = None
+            with self.section_lock:
+                self.current_section = None
 
-    def play(self):
-        try:
-            if not self.playing:
-                self.spotify.start_playback()
-                self.playing = True
-        except Exception as e:
-            self.error_queue.put(f"Error playing in Spotifizer: {str(e)}")
+        self.last_update_time = current_time
 
-    def pause(self):
+    def fetch_audio_analysis(self):
         try:
-            if self.playing:
-                self.spotify.pause_playback()
-                self.playing = False
+            analysis = self.spotify.audio_analysis(self.current_track_id)
+            with self.audio_analysis_lock:
+                self.audio_analysis = analysis
+            self.save_audio_analysis()
         except Exception as e:
-            self.error_queue.put(f"Error pausing in Spotifizer: {str(e)}")
+            self.error_queue.put(f"Error fetching audio analysis: {str(e)}")
 
-    def quiet(self, volume=50):
-        try:
-            self.spotify.volume(volume)
-        except Exception as e:
-            self.error_queue.put(f"Error setting volume in Spotifizer: {str(e)}")
+    def save_audio_analysis(self):
+        if not os.path.exists('song_data'):
+            os.makedirs('song_data')
+        
+        filename = f"song_data/{self.current_track_id}.json"
+        with open(filename, 'w') as f:
+            json.dump(self.audio_analysis, f)
 
-    def loud(self, volume=100):
-        try:
-            self.spotify.volume(volume)
-        except Exception as e:
-            self.error_queue.put(f"Error setting volume in Spotifizer: {str(e)}")
+    def update_current_section(self):
+        with self.audio_analysis_lock:
+            if self.audio_analysis and 'sections' in self.audio_analysis:
+                for section in self.audio_analysis['sections']:
+                    section_start = section['start'] * 1000
+                    section_end = section_start + (section['duration'] * 1000)
+                    if section_start <= self.progress < section_end:
+                        with self.section_lock:
+                            self.current_section = section
+                        break
 
-    def next(self):
+    def seek_to_timestamp(self, timestamp_ms):
         try:
-            self.spotify.next_track()
+            self.spotify.seek_track(timestamp_ms)
+            self.progress = timestamp_ms
         except Exception as e:
-            self.error_queue.put(f"Error playing next track in Spotifizer: {str(e)}")
+            self.error_queue.put(f"Error seeking to timestamp: {str(e)}")
 
-    def previous(self):
-        try:
-            self.spotify.previous_track()
-        except Exception as e:
-            self.error_queue.put(f"Error playing previous track in Spotifizer: {str(e)}")
+    def loop_current_section(self):
+        if self.current_section:
+            start_ms = int(self.current_section['start'] * 1000)
+            duration_ms = int(self.current_section['duration'] * 1000)
+            end_ms = start_ms + duration_ms
+
+            if self.progress >= end_ms:
+                self.seek_to_timestamp(start_ms)
 
     def stop(self):
         self.running.clear()
 
-
 def main():
-    stdscr = curses.initscr()
-    spotifizer = Spotifizer()
+    # Read the credentials from the file
+    credspath = os.path.join(os.path.dirname(__file__), '../../spotify_credentials.txt')
+    with open(credspath) as f:
+        lines = f.readlines()
+        client_id = lines[0].strip().split(' ')[1]
+        client_secret = lines[1].strip().split(' ')[1]
+        redirect_uri = lines[2].strip().split(' ')[1]
+
+    spotifizer = Spotifizer(client_id, client_secret, redirect_uri, update_interval=0.05, time_offset=1.0)
     spotifizer.start()
-    spotifizer.pause()
-    spotifizer.spotify.volume(50)
-    if not spotifizer.spotify.current_playback()['is_playing']:
-        spotifizer.play()
-    print('Listening to Spotify...')
+
     try:
         while True:
-            if spotifizer.current_track_info is not None:
-                stdscr.clear()
-                stdscr.addstr(0, 0, f"Track: {spotifizer.title}")
-                stdscr.addstr(1, 0, f"Artist: {spotifizer.artist}")
-                stdscr.addstr(2, 0, f"Playing: {spotifizer.playing}")
-                stdscr.addstr(3, 0, f"Volume: {spotifizer.volume}")
-                stdscr.addstr(4, 0, f"Progress_ms: {spotifizer.progress}")
-                stdscr.refresh()
+            if spotifizer.playing and spotifizer.current_track_info:
+                print(f"\nNow playing: {spotifizer.title} by {spotifizer.artist}")
+                with spotifizer.section_lock:
+                    current_section = spotifizer.current_section
+                if current_section:
+                    with spotifizer.audio_analysis_lock:
+                        if spotifizer.audio_analysis and 'sections' in spotifizer.audio_analysis:
+                            try:
+                                section_index = spotifizer.audio_analysis['sections'].index(current_section)
+                                total_sections = len(spotifizer.audio_analysis['sections'])
+                                print(f"Current section: {section_index + 1}/{total_sections}")
+                                print(f"Section start time: {current_section['start']:.2f}s")
+                                print(f"Section duration: {current_section['duration']:.2f}s")
+                                print(f"Current progress: {spotifizer.progress / 1000:.2f}s")
+                                print(f"Section loudness: {current_section['loudness']:.2f}dB")
+                                print(f"Section tempo: {current_section['tempo']:.2f} BPM")
+                                print(f"Section key: {current_section['key']}")
+                                print(f"Section mode: {'Major' if current_section['mode'] == 1 else 'Minor'}")
+                            except ValueError:
+                                print("Section information temporarily unavailable")
+                else:
+                    print("Waiting for section information...")
             else:
-                stdscr.addstr(0, 0, "No track playing...")
-                stdscr.refresh()
-
-    except KeyboardInterrupt:
-        spotifizer.stop()
-        print('Stopped listening to Spotify...')
-        time.sleep(1)
+                print("Waiting for track to start playing...")
+            
+            time.sleep(0.1)  # Update every 0.1 seconds
     
-    finally:
-
+    except KeyboardInterrupt:
+        print("\nStopping Spotifizer...")
+        spotifizer.stop()
         spotifizer.join()
-        curses.endwin()
+
+if __name__ == "__main__":
+    main()
 
 if __name__ == "__main__":
     main()
